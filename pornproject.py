8e06d1b97d10a7e05b8e801728f77d12de222bfb
avatarking <54052159+avatarking@users.noreply.github.com>
2019-08-13 12:22:54 +0700

    Add files via upload

---
+++ pornproject.py
@@ -0,0 +1,44 @@
+import os, sys
+print ("openwebsite")
+print ("")
+print ("credit: TING ")
+print ("")
+print
+print ("(1) xnxx ")
+print ("(2) pornhun ")
+print ("(3) ro89")
+print ("(4) rankaxxx")
+print
+print ("(0) exit ")
+print
+A = input ("openweb :")
+
+if A == "1" or A == "01":
+ print
+ os.system("termux-open-url https://xnxx.com/")
+ print
+ print
+
+elif A == "2" or A == "02":
+
+ print
+ os.system("termux-open-url https://pornhun.com/")
+ print
+ print
+ 
+elif A == "3" or A == "03":
+
+ print
+ os.system("termux-open-url https://ro89.com/")
+ print
+ print
+ 
+elif A == "4" or A == "04":
+
+ print 
+ os.system("termux-open-url https://www.rankaxxx.com/")
+ print
+ print
+
+elif A == "0" or A == "00":
+ sys.exit()
